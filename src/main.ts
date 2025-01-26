a17e9bc0898217dce3aadfaabd3a9f39075fc024
Gift Mugweni <giftmugweni@gmail.com>
2025-01-26 15:48:50 +0200

    working on deployment

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faBlog, faPersonDigging, faBookOpen } from '@fortawesome/free-solid-svg-icons'
 
-library.add(fas)
+library.add(faUser, faBlog, faPersonDigging, faBookOpen)
 
 createApp(App)
     .mount('#app')
