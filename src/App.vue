707dab54954d8ac2c7792928c8160da01ea5221d
Gift Mugweni <giftmugweni@gmail.com>
2025-02-01 09:16:41 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,6 @@
 <script setup lang="ts">
 import Navigation from './components/navigation/Navigation.vue';
 import { RouterView } from 'vue-router'
-
 import { OhVueIcon } from 'oh-vue-icons';
-
+console.log('')
 </script>
