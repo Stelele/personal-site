9677da3b3d70f658c85a41dc8f32825391d89107
Gift Mugweni <giftmugweni@gmail.com>
2025-02-12 06:10:37 +0000

    fixing build errors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,5 @@
 import { RouteRecordRaw } from "vue-router";
 import Overview from "../pages/home/Overview.vue";
-import CV from "../pages/home/CV.vue";
-import MyJourney from "../pages/home/MyJourney.vue";
 import WorkInProgress from "../pages/home/WorkInProgess.vue";
 
 export const routes: RouteRecordRaw[] = [
