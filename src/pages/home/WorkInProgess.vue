731709a9f57f1279ff8856ac9e5d8d8582567d23
Gift Mugweni <giftmugweni@gmail.com>
2025-02-12 06:07:58 +0000

    added work in progress page

---
+++ src/pages/home/WorkInProgess.vue
@@ -0,0 +1,10 @@
+<template>
+    <PageBase label="Work In Progress" icon="fc-opened-folder">
+        <Giphy class=" mt-3" :width="600" name="work in progress" />
+    </PageBase>
+</template>
+
+<script lang="ts" setup>
+import PageBase from '../../components/PageBase.vue';
+import Giphy from '../../components/Giphy.vue'
+</script>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { RouteRecordRaw } from "vue-router";
 import Overview from "../pages/home/Overview.vue";
 import CV from "../pages/home/CV.vue";
 import MyJourney from "../pages/home/MyJourney.vue";
+import WorkInProgress from "../pages/home/WorkInProgess.vue";
 
 export const routes: RouteRecordRaw[] = [
     {
@@ -11,14 +12,14 @@ export const routes: RouteRecordRaw[] = [
     },
     {
         path: "/cv",
-        component: CV,
+        component: WorkInProgress,
     },
     {
         path: "/my-journey",
-        component: MyJourney,
+        component: WorkInProgress,
     },
     {
         path: "/:catchAll(.*)",
-        redirect: "/",
+        component: WorkInProgress,
     }
 ] 
