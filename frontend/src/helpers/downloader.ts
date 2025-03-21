a0c93bfc5c224136d97bed89ac89a964715ed7dc
Gift Mugweni <giftmugweni@gmail.com>
2025-03-21 15:15:43 +0200

    working on build pipeline

--- frontend/src/helpers/downloader.ts
+++ frontend/src/helpers/downloader.ts
@@ -133,11 +133,3 @@ export async function getBlogFeeds() {
 
     return posts
 }
-
-function generateUID() {
-    const firstPart = (Math.random() * 46656) | 0;
-    const secondPart = (Math.random() * 46656) | 0;
-    const first = ("000" + firstPart.toString(36)).slice(-3);
-    const second = ("000" + secondPart.toString(36)).slice(-3);
-    return first + second;
-}
