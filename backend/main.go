0ed4fdae8cf2951b6625baefcd5d99f679fb0514
Gift Mugweni <giftmugweni@gmail.com>
2025-03-25 18:57:39 +0200

    testing build pipeline

--- backend/main.go
+++ backend/main.go
@@ -56,8 +56,9 @@ func handleGetFeed(w http.ResponseWriter, r *http.Request) {
 
 func addResponseHeaders(w *http.ResponseWriter) {
 	// CORS Stuff
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	
+	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 	(*w).Header().Add("Content-Type", "application/json")
