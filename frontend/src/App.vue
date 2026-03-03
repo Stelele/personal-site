4be1b747ee47026aa993d242e03aede59360c519
Stelele <giftmugweni@gmail.com>
2026-03-03 23:05:19 +0200

    removed commented out code sections

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,6 @@
 <template>
     <UApp>
         <UHeader>
-            <template #left>
-                <router-link to="/">
-                    <div class="w-full h-full flex gap-2 items-center p-1">
-                        <!-- <img
-                            src="/logo.png"
-                            class="object-contain h-20"
-                            alt="Njeremoto Logo"
-                        /> -->
-                    </div>
-                </router-link>
-            </template>
             <UPageHeader
                 :ui="{
                     title: 'text-xl sm:text-2xl',
@@ -20,16 +9,6 @@
             />
         </UHeader>
         <div class="flex flex-col min-h-screen">
-            <!-- <div class="flex sticky top-0 border-b z-10">
-                <div class="grow"></div>
-                <div
-                    class="w-80 text-center rounded-b-lg px-4 py-2 flex items-center justify-center gap-2"
-                >
-                    <UIcon name="i-heroicons-gift" class="w-5 h-5" />
-                    <span>Gift's Personal Site</span>
-                </div>
-                <div class="grow"></div>
-            </div> -->
             <UMain>
                 <Navigation>
                     <RouterView />
