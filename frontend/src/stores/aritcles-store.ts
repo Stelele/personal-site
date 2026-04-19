951f8270383ff31a0bda51b0c6a57f2a0cd8f313
Gift Mugweni <giftmugweni@gmail.com>
2026-04-19 13:14:01 +0200

    feat: articles store holds Blog[], adds helper methods

--- frontend/src/stores/aritcles-store.ts
+++ frontend/src/stores/aritcles-store.ts
@@ -1,25 +1,37 @@
 import { defineStore } from "pinia";
-import { Post } from "@/helpers/type";
+import { Blog } from "@/helpers/type";
 import { Ref, ref } from "vue";
 import { getBlogFeeds } from "@/helpers/downloader";
 
 export const useArticlesStore = defineStore("ArticlesStore", () => {
-  const posts = ref<Post[]>([]);
+  const blogs = ref<Blog[]>([]);
   const isDownloading = ref(true);
 
   function update() {
-    updatePosts(posts, isDownloading);
+    updatePosts(blogs, isDownloading);
+  }
+
+  function findPost(blogSlug: string, postId: string): Blog["posts"][0] | undefined {
+    const blog = blogs.value.find((b) => b.slug === blogSlug);
+    return blog?.posts.find((p) => p.id === postId);
+  }
+
+  function getPostsByBlog(blogSlug: string): Blog["posts"] {
+    const blog = blogs.value.find((b) => b.slug === blogSlug);
+    return blog?.posts ?? [];
   }
 
   return {
-    posts,
+    blogs,
     update,
     isDownloading,
+    findPost,
+    getPostsByBlog,
   };
 });
 
-async function updatePosts(posts: Ref<Post[]>, isDownloading: Ref<boolean>) {
+async function updatePosts(blogs: Ref<Blog[]>, isDownloading: Ref<boolean>) {
   isDownloading.value = true;
-  posts.value = await getBlogFeeds();
+  blogs.value = await getBlogFeeds();
   isDownloading.value = false;
 }
