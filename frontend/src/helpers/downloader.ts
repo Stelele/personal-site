80ae9a23a70cb52e793e013b2599fc7412eaf107
Gift Mugweni <giftmugweni@gmail.com>
2026-04-19 13:13:46 +0200

    feat: getBlogFeeds returns Blog[] with posts already grouped

--- frontend/src/helpers/downloader.ts
+++ frontend/src/helpers/downloader.ts
@@ -1,13 +1,12 @@
-import { Post } from "@/helpers/type";
+import { Blog, Post } from "@/helpers/type";
 import { getHashNodeFeed } from "@/helpers/blogs/hashnode";
 import moment from "moment";
 import { getMediumFeed } from "./blogs/medium";
 import { CmsService } from "@/services/cms";
 import { components } from "@/services/cms/schema";
 
-export async function getBlogFeeds() {
+export async function getBlogFeeds(): Promise<Blog[]> {
   const [mediumFeed, hashNodeFeed] = await Promise.all([getMediumFeed(), getHashNodeFeed()]);
-  const posts: Post[] = [...mediumFeed, ...hashNodeFeed];
 
   const cmsClient = await CmsService.getClient();
   const cmsBlogs = await cmsClient.GET('/blogs', {
@@ -15,6 +14,52 @@ export async function getBlogFeeds() {
       query: {
         slugs: ["progamming", "walking", "random"]
       }
+    }
+  })
+
+  const cmsBlogList: Blog[] = [];
+  for (const blog of cmsBlogs.data ?? []) {
+    if (!blog.id) continue;
+    const blogPosts = await cmsClient.GET('/blogs/{blogId}/posts', {
+      params: {
+        path: {
+          blogId: blog.id
+        },
+      },
+    });
+    
+    const posts: Post[] = [];
+    for (const post of blogPosts.data ?? []) {
+      posts.push({
+        id: post.id ?? '',
+        title: post.title ?? '',
+        brief: post.description ?? '',
+        link: post.slug ?? '',
+        coverImage: post.coverImageUrl ?? '',
+        publishDate: post.publishedOn ?? '',
+        updateDate: post.updatedOn ?? '',
+        tags: [post.tag ?? ''],
+        content: post.content ?? '',
+      });
+    }
+
+    cmsBlogList.push({
+      id: blog.id,
+      name: blog.title ?? 'Untitled',
+      slug: blog.slug ?? blog.id,
+      icon: "i-heroicons-book-open",
+      posts: posts,
+    });
+  }
+
+  const allBlogs: Blog[] = [mediumFeed, hashNodeFeed, ...cmsBlogList];
+
+  for (const blog of allBlogs) {
+    blog.posts.sort((a, b) => (moment(a.publishDate).isBefore(moment(b.publishDate)) ? 1 : -1));
+  }
+
+  return allBlogs;
+}
     }
   })
   const cmsBlogPosts: components['schemas']['PostResponse'][] = [];
